65aa1374bca0ff8a18c284c205516944720c69fb
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-03-10 16:05:30 +0000

    utils - core layer

--- app/src/main/java/br/com/movieapp/core/data/remote/response/MovieResponse.kt
+++ app/src/main/java/br/com/movieapp/core/data/remote/response/MovieResponse.kt
@@ -1,13 +1,13 @@
-package br.com.movieapp.framework.data.remote.response
+package br.com.movieapp.core.data.remote.response
 
-import br.com.movieapp.framework.data.remote.model.SearchResult
+import br.com.movieapp.core.data.remote.model.MovieResult
 import com.google.gson.annotations.SerializedName
 
-data class SearchResponse(
+data class MovieResponse(
     @SerializedName("page")
     val page: Int,
     @SerializedName("results")
-    val searchResults: List<SearchResult>,
+    val results: List<MovieResult>,
     @SerializedName("total_pages")
     val totalPages: Int,
     @SerializedName("total_results")
--- app/src/main/java/br/com/movieapp/core/data/remote/response/SearchResponse.kt
+++ app/src/main/java/br/com/movieapp/core/data/remote/response/SearchResponse.kt
@@ -1,13 +1,13 @@
-package br.com.movieapp.framework.data.remote.response
+package br.com.movieapp.core.data.remote.response
 
-import br.com.movieapp.framework.data.remote.model.MovieResult
+import br.com.movieapp.core.data.remote.model.SearchResult
 import com.google.gson.annotations.SerializedName
 
-data class MovieResponse(
+data class SearchResponse(
     @SerializedName("page")
     val page: Int,
     @SerializedName("results")
-    val results: List<MovieResult>,
+    val searchResults: List<SearchResult>,
     @SerializedName("total_pages")
     val totalPages: Int,
     @SerializedName("total_results")
