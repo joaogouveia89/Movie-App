3ab9b2005e9ff63a68589b10494dc143611995e4
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-03-12 21:34:04 +0000

    implementation of GetMovieDetailsUseCase

---
+++ app/src/main/java/br/com/movieapp/movieDetailFeature/domain/usecase/GetMovieDetailsUseCase.kt
@@ -0,0 +1,48 @@
+package br.com.movieapp.movieDetailFeature.domain.usecase
+
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import br.com.movieapp.core.domain.model.Movie
+import br.com.movieapp.core.domain.model.MovieDetails
+import br.com.movieapp.core.util.ResultData
+import br.com.movieapp.movieDetailFeature.domain.repository.MovieDetailsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+interface GetMovieDetailsUseCase {
+    operator fun invoke(params: Params): Flow<ResultData<Pair<Flow<PagingData<Movie>>, MovieDetails>>>
+    data class Params(val movieId: Int)
+}
+
+
+class GetMovieDetailsUseCaseImpl @Inject constructor(
+    private val repository: MovieDetailsRepository
+): GetMovieDetailsUseCase{
+    override fun invoke(params: GetMovieDetailsUseCase.Params): Flow<ResultData<Pair<Flow<PagingData<Movie>>, MovieDetails>>> =
+        flow {
+            try {
+                emit(ResultData.Loading)
+                val movieDetails = repository.getMovieDetails(params.movieId)
+                val moviesSimilar = repository.getMoviesSimilar(
+                    movieId = params.movieId,
+                    pagingConfig = PagingConfig(
+                        pageSize = 20,
+                        initialLoadSize = 20
+                    )
+                )
+
+                emit(ResultData.Success(moviesSimilar to movieDetails))
+            }catch (exception: IOException) {
+                emit(ResultData.Failure(exception))
+            } catch (exception: HttpException) {
+                emit(ResultData.Failure(exception))
+            }
+        }.flowOn(Dispatchers.IO)
+
+}
