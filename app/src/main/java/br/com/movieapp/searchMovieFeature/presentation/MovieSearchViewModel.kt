b5ba89f79ed84f90096ef568bbc0746772b44c14
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-03-12 19:40:56 +0000

    implementation of viewmodel

---
+++ app/src/main/java/br/com/movieapp/searchMovieFeature/presentation/MovieSearchViewModel.kt
@@ -0,0 +1,38 @@
+package br.com.movieapp.searchMovieFeature.presentation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import br.com.movieapp.searchMovieFeature.domain.usecase.GetMovieSearchUseCase
+import br.com.movieapp.searchMovieFeature.presentation.state.MovieSearchState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieSearchViewModel @Inject constructor(
+    private val getMovieSearchUseCase: GetMovieSearchUseCase
+) : ViewModel(){
+    var uiState by mutableStateOf(MovieSearchState())
+        private set
+
+    fun fetch(query: String = ""){
+        val movies = getMovieSearchUseCase.invoke(
+            params = GetMovieSearchUseCase.Params(
+                query = query
+            )
+        ).cachedIn(viewModelScope)
+
+        uiState = uiState.copy(movies = movies)
+    }
+
+    fun event(event: MovieSearchEvent){
+        uiState = when(event){
+            is MovieSearchEvent.EnteredQuery -> {
+                uiState.copy(query = event.value)
+            }
+        }
+    }
+}
