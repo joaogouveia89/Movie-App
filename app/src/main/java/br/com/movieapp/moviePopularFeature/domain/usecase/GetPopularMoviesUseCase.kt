c661301065e72b829849e4a27bd58650ced44b97
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-03-12 14:07:02 +0000

    IMplementing GetPopularMoviesUseCase

---
+++ app/src/main/java/br/com/movieapp/moviePopularFeature/domain/usecase/GetPopularMoviesUseCase.kt
@@ -0,0 +1,25 @@
+package br.com.movieapp.moviePopularFeature.domain.usecase
+
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import br.com.movieapp.core.domain.model.Movie
+import br.com.movieapp.moviePopularFeature.domain.repository.MoviePopularRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface GetPopularMoviesUseCase {
+    operator fun invoke(): Flow<PagingData<Movie>>
+}
+
+class GetPopularMoviesUseCaseImpl @Inject constructor(
+    private val repository: MoviePopularRepository
+): GetPopularMoviesUseCase{
+    override fun invoke(): Flow<PagingData<Movie>> =
+        repository.getPopularMovies(
+            pagingConfig = PagingConfig(
+                pageSize = 20,
+                initialLoadSize = 20
+            )
+        )
+
+}
