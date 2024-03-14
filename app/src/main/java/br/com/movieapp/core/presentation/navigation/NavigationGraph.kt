baeb524520bc5fe9e74bf7b8b614dc1b10c6d40a
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-03-14 23:27:40 +0000

    implementation of MovieDetailScreen

--- app/src/main/java/br/com/movieapp/core/presentation/navigation/NavigationGraph.kt
+++ app/src/main/java/br/com/movieapp/core/presentation/navigation/NavigationGraph.kt
@@ -1,9 +1,14 @@
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import br.com.movieapp.core.presentation.navigation.BottomNavItem
+import br.com.movieapp.core.util.Constants.MOVIE_DETAIL_ARGUMENT_KEY
+import br.com.movieapp.movieDetailFeature.presentation.MovieDetailScreen
+import br.com.movieapp.movieDetailFeature.presentation.MovieDetailViewModel
 import br.com.movieapp.moviePopularFeature.presentation.MoviePopularScreen
 import br.com.movieapp.moviePopularFeature.presentation.MoviePopularViewModel
 import br.com.movieapp.searchMovieFeature.presentation.MovieSearchEvent
@@ -22,7 +27,9 @@ fun NavigationGraph(navController: NavHostController) {
 
             MoviePopularScreen(
                 uiState = uiState,
-                navigateToDetailMovie = {})
+                navigateToDetailMovie = {
+                    navController.navigate(BottomNavItem.MovieDetails.passMovieId(it))
+                })
         }
         composable(BottomNavItem.MovieSearch.route) {
             val viewModel: MovieSearchViewModel = hiltViewModel()
@@ -35,12 +42,32 @@ fun NavigationGraph(navController: NavHostController) {
                 onEvent = onEvent,
                 onFetch = onFetch,
                 navigateToDetailMovie = {
-
+                    navController.navigate(BottomNavItem.MovieDetails.passMovieId(it))
                 }
             )
         }
         composable(BottomNavItem.MovieFavorite.route) {
 
+        }
+
+        composable(
+            route = BottomNavItem.MovieDetails.route,
+            arguments = listOf(
+                navArgument(MOVIE_DETAIL_ARGUMENT_KEY){
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ){
+            val viewModel: MovieDetailViewModel = hiltViewModel()
+            val uiState = viewModel.uiState
+            val getMovieDetail = viewModel::getMovieDetail
+
+            MovieDetailScreen(
+                id = it.arguments?.getInt(MOVIE_DETAIL_ARGUMENT_KEY),
+                uiState = uiState,
+                getMovieDetail = getMovieDetail
+            )
         }
     }
 }
---
+++ app/src/main/java/br/com/movieapp/movieDetailFeature/presentation/MovieDetailScreen.kt
@@ -0,0 +1,59 @@
+package br.com.movieapp.movieDetailFeature.presentation
+
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.res.stringResource
+import androidx.paging.compose.collectAsLazyPagingItems
+import br.com.movieapp.R
+import br.com.movieapp.movieDetailFeature.presentation.components.MovieDetailContent
+import br.com.movieapp.movieDetailFeature.presentation.state.MovieDetailState
+import br.com.movieapp.ui.theme.black
+import br.com.movieapp.ui.theme.white
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MovieDetailScreen(
+    id: Int?,
+    uiState: MovieDetailState,
+    getMovieDetail: (MovieDetailEvent.GetMovieDetail) -> Unit
+) {
+    val pagingMoviesSimilar = uiState.results.collectAsLazyPagingItems()
+
+    // creates a separate flow
+    LaunchedEffect(key1 = true) {
+        if(id != null){
+            getMovieDetail(MovieDetailEvent.GetMovieDetail(id))
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.detail_movie),
+                        color = white
+                    )
+                },
+                backgroundColor = black
+            )
+        },
+        content = {
+            // TODO: Test here call the block of code inside the LaunchedEffect
+            MovieDetailContent(
+                movieDetails = uiState.movieDetails,
+                pagingMoviesSimilar = pagingMoviesSimilar,
+                isLoading = uiState.isLoading,
+                isError = uiState.error,
+                iconColor = uiState.iconColor,
+                onAddFavorite = { movie ->
+
+                }
+            )
+        }
+    )
+}
